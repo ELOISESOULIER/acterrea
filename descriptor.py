f68eda1af7340d3d60134a8b56d90a9c0ffa7fca
ELOISESOULIER <eloise.soulier@gmail.com>
2018-05-24 14:49:14 +0200

    new descriptors

--- descriptor.py
+++ descriptor.py
@@ -1,5 +1,14 @@
 import numpy as np
 
+def spectral_moment(V,i):
+    
+    fft = abs(np.fft.fft(V))
+    freq = np.fft.fftfreq(len(V))
+    
+    sm = np.sum(fft*(freq**i))/np.sum(fft)
+    
+    return sm
+
 def compute_descriptor(V):
     """ Return a list of descriptors of the signal V """
 
@@ -12,5 +21,22 @@ def compute_descriptor(V):
     descriptors.append(fr_max)
     ampl_max = np.max(signal)
     descriptors.append(ampl_max)
+    
+    mu1 = spectral_moment(V,1)
+    mu2 = spectral_moment(V,2)
+    mu3 = spectral_moment(V,3)
+    mu4 = spectral_moment(V,4)
+    
+    SC = mu1 #spectral centroid
+    descriptors.append(SC)
+    SL = np.sqrt(mu2 - mu1**2) #spectral width
+    descriptors.append(SL)
+    SP = (-3*mu1**4 + 6*mu1*mu2 - 4*mu1*mu3 +mu4)/SL**4 - 3 #spectral platitude
+    descriptors.append(SP)
+    
 
     return np.array(descriptors)
+
+
+
+    
